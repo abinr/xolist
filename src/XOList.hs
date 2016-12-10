08f1d2e22b1b779bb3fdc62ab92815a1bb20c7d4
apotropaix <apotropaix@apotropaix.me>
2016-12-10 14:04:18 +0100

    first commit'

---
+++ src/XOList.hs
@@ -0,0 +1,40 @@
+module XOList where
+
+import Data.Monoid
+import Control.Monad
+
+data List a
+  = Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Monoid (List a) where
+  mempty =
+    Nil
+  mappend xs ys =
+    case (xs, ys) of
+      (Nil, ys) ->
+        ys
+      (Cons x xs, ys) ->
+        Cons x $ mappend xs ys
+
+
+instance Functor List where
+  fmap f s =
+    case s of
+      Nil ->
+        Nil
+      Cons x xs ->
+        Cons (f x) (fmap f xs)
+
+instance Applicative List where
+  pure x = Cons x Nil
+  (<*>) fs vs =
+    case (fs, vs) of
+      (Nil, _) ->
+        Nil
+      (_, Nil) ->
+        Nil
+      (Cons hd tl, ys) ->
+        mappend (fmap hd ys) ((<*>) tl ys)
+
---
+++ test/Spec.hs
@@ -0,0 +1,25 @@
+import XOList
+import Control.Monad
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+instance (Eq a) => EqProp (List a) where
+  (=-=) =
+    eq
+
+instance (Arbitrary a) => Arbitrary (List a) where
+  arbitrary =
+    listGen
+
+{-- Perhaps aive, risks to never terminate --}
+listGen :: (Arbitrary a) => Gen (List a)
+listGen =
+  oneof [return Nil, liftM2 Cons arbitrary listGen]
+
+main :: IO ()
+main = do
+  quickBatch $ monoid (undefined :: List String)
+  quickBatch $ functor (undefined :: List (Int, String, Char))
+  quickBatch $ applicative (undefined :: List (Int, String, Char))
+
